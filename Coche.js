3b208ec03f42364ff770e2a6ee46214069b783f2
YULY ANDREA PEREZ GUZMAN <perezya@HILDEBOG.LOCAL>
2020-12-02 00:01:01 -0500

    Clase coche

---
+++ Coche.js
@@ -0,0 +1,27 @@
+export class Coche{
+    
+  Coche(marca){
+      this.marca = marca;
+  }
+
+  getMarcaCoche() {
+      return marca;
+  }
+
+}
+
+export class CocheBD{
+
+    guardarCocheBD(){
+
+    }
+
+    eliminarCoche(){
+
+    }
+}
+
+
+
+
+
